98d0f49c1d2a4b09600b7cf0399e6a4273c28cdc
Jorge Trad <jetradl@gmail.com>
2022-07-10 09:51:15 -0500

    What is a promise

---
+++ src/promise/index.js
@@ -0,0 +1,24 @@
+const promise = new Promise(function (resolve, reject) {
+  resolve();
+});
+
+const cows = 15;
+
+const countCows = new Promise(function (resolve, reject) {
+  if (cows > 10) {
+    resolve(`We've ${cows} cows in the farm`);
+  } else {
+    reject('There is no cows on the farm');
+  }
+});
+
+countCows
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+  .finally(() => {
+    console.log('Finally');
+  });
